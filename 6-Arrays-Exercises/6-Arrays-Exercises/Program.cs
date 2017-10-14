283db75d579b54199b7601de0d850f91d12b17ca
SShope <sshope19@gmail.com>
2017-10-14 21:14:20 +0300

    Arrays

---
+++ 6-Arrays-Exercises/6-Arrays-Exercises/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_Arrays_Exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] inputArray = Console.ReadLine().Split(' ').Select(int.Parse).ToArray(); ;
+            int rotation = int.Parse(Console.ReadLine());
+
+            int[] resultArray = RotateArray(inputArray, rotation);
+
+            for (int i = 0; i < resultArray.Length; i++)
+            {
+                Console.Write($"{resultArray[i]} ");
+            }
+        }
+
+        static int[] RotateArray(int[] array, int rotation)
+        {
+
+            int arrLenght = array.Length;
+            int lastEl;
+
+            int[] rotatedArray = array;
+            int[] finalArray = new int[arrLenght];
+
+            for (int i = 0; i < rotation; i++)
+            {
+                lastEl = rotatedArray[arrLenght - 1];
+                for (int j = arrLenght - 1 ; j >= 1; j--)
+                {
+                    rotatedArray[j] = rotatedArray[j-1];
+                }
+                rotatedArray[0] = lastEl;
+                finalArray = SumArrays(rotatedArray, finalArray);
+            }
+
+            return finalArray;
+        }
+
+        static int[] SumArrays(int[] firstArray, int[] secondArray)
+        {
+            int[] returnArray = new int[firstArray.Length];
+            for (int i = 0; i < firstArray.Length; i++)
+            {
+                returnArray[i] = firstArray[i] + secondArray[i];
+            }
+            return returnArray;
+        }
+
+    }
+}
---
+++ 6-Arrays-Exercises/6_3-FoldandSum/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_3_FoldandSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] inputArr = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+
+
+
+        }
+
+        static int[] foldArray(int[] inputArr)
+        {
+            int[] leftArray = new int[inputArr.Length / 4];
+            int[] rightArray = new int[inputArr.Length / 4];
+            int[] innerArray = new int[inputArr.Length / 2];
+
+            // leftArray elements + reverse
+            for (int i = 0; i < (inputArr.Length / 4) - 1; i++)
+            {
+                leftArray[i] = innerArray[i];
+            }
+            Array.Reverse(leftArray);
+
+            // rightArray elements + reverse
+            for (int i = inputArr.Length; i >= (3 * inputArr.Length / 4) - 1; i--)
+            {
+                rightArray[];
+            }
+            Array.Reverse(rightArray);
+
+        }
+
+    }
+}
